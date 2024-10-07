a77107ab239a8133d639c765848cd6db45ca70b1
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-07 15:01:49 -0300

    Implementada navbar inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+
+import { ShellComponent } from './core/components/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [ShellComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
