4c5fa078ce48c3b52623db6e92332b2c6dcf9a6e
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-09 17:15:59 -0300

    Removidos imports obsoletos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { ListagemCategoriasComponent } from './views/categorias/listar/listagem-categorias.component';
-import { CadastroCategoriaComponent } from './views/categorias/cadastrar/cadastro-categoria.component';
-import { EdicaoCategoriaComponent } from './views/categorias/editar/edicao-categoria.component';
-import { ExclusaoCategoriaComponent } from './views/categorias/excluir/exclusao-categoria.component';
 import { notasRoutes } from './views/notas/notas.routes';
 import { categoriasRoutes } from './views/categorias/categorias.routes';
 
