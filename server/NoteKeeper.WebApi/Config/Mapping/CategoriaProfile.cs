2c4a4bff1efd4fcde629ff02684d53f8351f4f3e
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-11-06 14:46:04 -0300

    Implementado controlador de categorias completo

--- server/NoteKeeper.WebApi/Config/Mapping/CategoriaProfile.cs
+++ server/NoteKeeper.WebApi/Config/Mapping/CategoriaProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using NoteKeeper.Dominio.ModuloCategoria;
+using NoteKeeper.Dominio.ModuloNota;
 using NoteKeeper.WebApi.ViewModels;
 
 namespace NoteKeeper.WebApi.Config.Mapping;
@@ -9,8 +10,11 @@ public class CategoriaProfile : Profile
     public CategoriaProfile()
     {
         CreateMap<Categoria, ListarCategoriaViewModel>();
+        CreateMap<Categoria, VisualizarCategoriaViewModel>();
 
         CreateMap<InserirCategoriaViewModel, Categoria>();
         CreateMap<EditarCategoriaViewModel, Categoria>();
+
+        CreateMap<Nota, ListarNotaViewModel>();
     }
 }
---
+++ server/NoteKeeper.WebApi/ViewModels/NotaViewModels.cs
@@ -0,0 +1,7 @@
+﻿namespace NoteKeeper.WebApi.ViewModels;
+
+public class ListarNotaViewModel
+{
+    public required Guid Id { get; set; }
+    public required string Titulo { get; set; }
+}
